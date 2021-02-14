03352b1a4305af6ce70c63273dc35ec440dc3f83
Jalilov <javohirjalilov777@gmail.com>
2021-02-14 21:13:27 +0500

    insert data

--- db.py
+++ db.py
@@ -1,1 +1,10 @@
-import tinydb
+from tinydb import TinyDB
+db = TinyDB('db.json')
+
+user1 = {'Name':'Javohir','Age':21,'City':'Samarqand','Job':'Developer'}
+user2 = {'Name':'Jamshid','Age':19,'City':'Jizzax','Job':'Coding'}
+user3 = {'Name':'Diyor','Age':19,'City':'Jizzax','Job':'Software engeener'}
+
+db.insert(user1)
+db.insert(user2)
+db.insert(user3)
